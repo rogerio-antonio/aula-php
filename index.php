21fbca07f4f110c9b0fcbef7a6ef085520555096
rafael cavalcante <rafaelcavalcantee1978@gmail.com>
2025-06-25 21:37:04 -0300

    Adicionados os primeiros arquivos: form, config e index

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=+, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <nav>
+        <a href="views/form.php"></a>
+    </nav>
+
+
+
+
+
+</body>
+</html>
---
+++ views/form.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form</title>
+</head>
+<body>
+    <h1>Aula de Formulário</h1>
+
+    <form action="">
+        <div>
+            <label for="nome">Insira seu nome:</label>
+            <input type="number" placeholder="Digite seu nome" name="nome" id="nome">
+        </div>
+
+         <div>
+            <label for="idade">Insira sua idade:</label>
+            <input type="number" placeholder="Digite sua idade" name="idade" id="idade">
+        </div>
+
+        <div>
+            <label for="estado">Selecione seu estado:</label>
+            <select name="estado" id="estado">
+                <option value="mg">Minas Gerais</option>
+                <option value="sp">São Paulo</option>
+                <option value="rj">Rio de Janeiro</option>
+            </select>
+        </div>
+
+        <div>
+            <button tupe="submit">Enviar</button>
+        </div>
+    </form>
+    
+
+
+
+
+
+
+
+</body>
+</html>
